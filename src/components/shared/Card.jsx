d48dc3bb446022d9bd89a819609b4b96819f1798
cmomodo <ceesay.ml@outlook.com>
2022-05-23 15:52:57 +0100

    First one

---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+
+function Card({children,reverse}){
+    // return <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+/* A ternary operator. */
+
+    return (<div className="card" style={{backgroundColor: reverse ? 'rgba(0,0,0,0,0.4)' : '#fff', color:reverse ? '#fff' : '#000',}}>{children}</div>)
+}
+
+Card.defaultProps = {reverse: false,}
+Card.propTypes ={children: PropTypes.node.isRequired,
+reverse: PropTypes.bool}
+
+export default Card;
