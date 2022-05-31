cb8ac863fd639c31daad873da21802b528fef449
cmomodo <ceesay.ml@outlook.com>
2022-05-31 21:28:57 +0100

    This is the final project

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,133 +1,132 @@
-import {useState} from 'react'
+import {useState, useContext, useEffect} from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
 function RatingSelect({select}) {
     const [selected, setSelected] = useState(10)
+const {feedbackEdit} = useContext(FeedbackContext)
 
+useEffect(() => {
+    setSelected(feedbackEdit.item.rating)
+},[feedbackEdit])
     const handleChange = (e) => {
-        setSelected(+e.currentTarget.value)
-        select(+e.currentTarget.value)
-    }
-  return (
+        setSelected(+e.currentTarget.value) 
+        select(e.currentTarget.value)}
+ return (
     <ul className='rating'>
-      (
         <li>
-          <input
+            <input
             type='radio'
-            id={`num1`}
+            id='num1'
             name='rating'
-            value="1"
+            value = '1'
             onChange={handleChange}
             checked={selected === 1}
           />
-          <label htmlFor={`num1`}>1</label>
+          <label htmlFor='num1'>1</label>
         </li>
-
         <li>
-          <input
+            <input
             type='radio'
-            id={`num2`}
+            id='num2'
             name='rating'
-            value="2"
+            value = '2'
             onChange={handleChange}
             checked={selected === 2}
           />
-          <label htmlFor={`num2`}>2</label>
+          <label htmlFor='num2'>2</label>
         </li>
-
         <li>
-          <input
+            <input
             type='radio'
-            id={`num3`}
+            id='num3'
             name='rating'
-            value="3"
+            value = '3'
             onChange={handleChange}
             checked={selected === 3}
           />
-          <label htmlFor={`num3`}>3</label>
+          <label htmlFor='num3'>3</label>
         </li>
-
         <li>
-          <input
+            <input
             type='radio'
-            id={`num4`}
+            id='num4'
             name='rating'
-            value="4"
+            value = '4'
             onChange={handleChange}
             checked={selected === 4}
           />
-          <label htmlFor={`num4`}>4</label>
+          <label htmlFor='num4'>4</label>
         </li>
-
         <li>
-          <input
+            <input
             type='radio'
-            id={`num5`}
+            id='num5'
             name='rating'
-            value="5"
+            value = '5'
             onChange={handleChange}
             checked={selected === 5}
           />
-          <label htmlFor={`num5`}>5</label>
+          <label htmlFor='num1'>1</label>
         </li>
         <li>
-          <input
+            <input
             type='radio'
-            id={`num6`}
+            id='num6'
             name='rating'
-            value="6"
+            value = '6'
             onChange={handleChange}
             checked={selected === 6}
           />
-          <label htmlFor={`num6`}>6</label>
+          <label htmlFor='num1'>7</label>
         </li>
         <li>
-          <input
+            <input
             type='radio'
-            id={`num7`}
+            id='num7'
             name='rating'
-            value="7"
+            value = '7'
             onChange={handleChange}
             checked={selected === 7}
           />
-          <label htmlFor={`num7`}>7</label>
+          <label htmlFor='num7'>7</label>
         </li>
         <li>
-          <input
+            <input
             type='radio'
-            id={`num8`}
+            id='num8'
             name='rating'
-            value="8"
+            value = '8'
             onChange={handleChange}
             checked={selected === 8}
           />
-          <label htmlFor={`num8`}>8</label>
+          <label htmlFor='num8'>8</label>
         </li>
         <li>
-          <input
+            <input
             type='radio'
-            id={`num9`}
+            id='num9'
             name='rating'
-            value="9"
+            value = '9'
             onChange={handleChange}
             checked={selected === 9}
           />
-          <label htmlFor={`num9`}>9</label>
+          <label htmlFor='num9'>9</label>
         </li>
-
         <li>
-          <input
+            <input
             type='radio'
-            id={`num10`}
+            id='num10'
             name='rating'
-            value="10"
+            value = '10'
             onChange={handleChange}
             checked={selected === 10}
           />
-          <label htmlFor={`num10`}>10</label>
+          <label htmlFor='num10'>10</label>
         </li>
-      )
-    </ul>
+        </ul>
   )
 }
 
+
+ 
 export default RatingSelect
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+import Card from "../components/shared/Card"
+function AboutPage() {
+  return (
+    <div className="about">
+      <div>AboutPage</div>
+    <p>This is a react app to leave feedback for a product or </p>
+    <p>Version: 1.0.0</p>
+    </div>
+    
+  )
+}
+
+export default AboutPage
